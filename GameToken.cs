eba144ac0a3612f576e6e168afcf9ee09770e517
Vadim Agishev <vadim.agishev@gmail.com>
2018-09-26 12:02:09 +0500

    Fix #1

--- GameToken.cs
+++ GameToken.cs
@@ -6,8 +6,8 @@ namespace Expload {
     [Program]
     public class GameToken {
 
-        public Mapping<Bytes, double> Balance = 
-            new Mapping<Bytes, double>();
+        public Mapping<Bytes, UInt32> Balance = 
+            new Mapping<Bytes, UInt32>();
 
         public Mapping<Bytes, int> WhiteList =
             new Mapping<Bytes, int>();
@@ -19,17 +19,17 @@ namespace Expload {
         }
 
         // Gives amount of GameTokens to recipient.
-        public void Give(Bytes recipient, double amount) {
+        public void Give(Bytes recipient, UInt32 amount) {
             assertIsOwner();
-            double lastBalance = Balance.getDefault(recipient, 0);
-            double newBalance = lastBalance + amount;
+            UInt32 lastBalance = Balance.getDefault(recipient, 0);
+            UInt32 newBalance = lastBalance + amount;
             Balance.put(recipient, newBalance);
         }
 
         // Remove amount of GameTokens from balance of address.
-        public void Burn(Bytes address, double amount) {
+        public void Burn(Bytes address, UInt32 amount) {
             assertIsOwner();
-            double balance = Balance.getDefault(address, 0);
+            UInt32 balance = Balance.getDefault(address, 0);
             if (balance >= amount) {
                 Balance.put(address, balance - amount);
             } else {
@@ -50,17 +50,17 @@ namespace Expload {
         }
 
         // Check address is white listed/
-        public bool WhiteListCheck(Bytes address) {
+        private bool WhiteListCheck(Bytes address) {
             return WhiteList.getDefault(address, 0) == 1;
         }
 
         // Send GameTokens from transaction Sender to recipient.
         // Recipient should be present in the Game Developers white list.
-        public void Spend(Bytes recipient, double amount) {
+        public void Spend(Bytes recipient, UInt32 amount) {
             if (WhiteListCheck(recipient)) {
                 Bytes sender = Info.Sender();
-                double senderBalance = Balance.getDefault(sender, 0);
-                double recipientBalance = Balance.getDefault(recipient, 0);
+                UInt32 senderBalance = Balance.getDefault(sender, 0);
+                UInt32 recipientBalance = Balance.getDefault(recipient, 0);
                 if (senderBalance >= amount) {
                     Balance.put(sender, senderBalance - amount);
                     Balance.put(recipient, recipientBalance + amount);
@@ -69,5 +69,6 @@ namespace Expload {
                 }
             }
         }
+        public static void Main() { }
     }
 }
