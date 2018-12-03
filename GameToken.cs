37a3afe9f09d44f4390f9e9faf0c149fabb2cf68
Vadim Agishev <vadim.agishev@gmail.com>
2018-12-03 17:52:38 +0500

    Integrate changes in `Mapping` methods resolves #15

--- GameToken.cs
+++ GameToken.cs
@@ -7,26 +7,26 @@ namespace Expload {
     public class GameToken {
         public static void Main() { }
 
-        public Mapping<Bytes, UInt32> Balance = 
+        private Mapping<Bytes, UInt32> Balance = 
             new Mapping<Bytes, UInt32>();
 
-        public Mapping<Bytes, int> WhiteList =
+        private Mapping<Bytes, int> WhiteList =
             new Mapping<Bytes, int>();
         
         // Gives amount of GameTokens to recipient.
         public void Give(Bytes recipient, UInt32 amount) {
             assertIsOwner();
-            UInt32 lastBalance = Balance.getDefault(recipient, 0);
+            UInt32 lastBalance = Balance.GetOrDefault(recipient, 0);
             UInt32 newBalance = lastBalance + amount;
-            Balance.put(recipient, newBalance);
+            Balance[recipient] = newBalance;
         }
 
         // Remove amount of GameTokens from balance of address.
         public void Burn(Bytes address, UInt32 amount) {
             assertIsOwner();
-            UInt32 balance = Balance.getDefault(address, 0);
+            UInt32 balance = Balance.GetOrDefault(address, 0);
             if (balance >= amount) {
-                Balance.put(address, balance - amount);
+                Balance[address] = balance - amount;
             } else {
                 Error.Throw("Not enough funds");
             }
@@ -35,19 +35,19 @@ namespace Expload {
         // Add address to white list
         public void WhiteListAdd(Bytes address) {
             assertIsOwner();
-            WhiteList.put(address, 1);
+            WhiteList[address] = 1;
         }
 
         // Remove address from white list.
         public void WhiteListRemove(Bytes address) {
             assertIsOwner();
-            WhiteList.put(address, 0);
+            WhiteList[address] = 0;
         }
 
         public UInt32 MyBalance()
         {
             Bytes sender = Info.Sender();
-            UInt32 senderBalance = Balance.getDefault(sender, 0);
+            UInt32 senderBalance = Balance.GetOrDefault(sender, 0);
             return senderBalance;
         }
 
@@ -56,11 +56,11 @@ namespace Expload {
         public void Spend(Bytes recipient, UInt32 amount) {
             if (WhiteListCheck(recipient)) {
                 Bytes sender = Info.Sender();
-                UInt32 senderBalance = Balance.getDefault(sender, 0);
-                UInt32 recipientBalance = Balance.getDefault(recipient, 0);
+                UInt32 senderBalance = Balance.GetOrDefault(sender, 0);
+                UInt32 recipientBalance = Balance.GetOrDefault(recipient, 0);
                 if (senderBalance >= amount) {
-                    Balance.put(sender, senderBalance - amount);
-                    Balance.put(recipient, recipientBalance + amount);
+                    Balance[sender] = senderBalance - amount;
+                    Balance[recipient] = recipientBalance + amount;
                     Log.Event("GameToken:Spend", new SpendEventData(recipient, amount));
                 } else {
                     Error.Throw("GameTokenError: Not enough funds for Spend operation");
@@ -73,7 +73,7 @@ namespace Expload {
         // Check address is white listed/
         private bool WhiteListCheck(Bytes address)
         {
-            return WhiteList.getDefault(address, 0) == 1;
+            return WhiteList.GetOrDefault(address, 0) == 1;
         }
 
         private void assertIsOwner()
